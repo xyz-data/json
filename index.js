e8998a1c1f6118f9830bf41e4d4b4bf6bc714a37
xgqfrms-gildata <xiagq@gildata.com>
2017-06-26 11:10:10 +0800

    update fetch & json @/#

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let url = `https://raw.githubusercontent.com/gildata/json/master/datas/ntb-F9/csjj.json`;
+let url = `https://raw.githubusercontent.com/gildata/json/master/datas/ntb-F9/csjj-old.json`;
 
 fetch(url)
 .then((response) => response.json())
@@ -17,6 +17,39 @@ fetch(url)
     let name = "@name";
     console.log(`name = ${repos.name}`);
 });
+
+
+
+url = `https://raw.githubusercontent.com/gildata/json/master/datas/ntb-F9/csjj.json`;
+
+fetch(url)
+.then((response) => response.json())
+.then((json)=> {
+    console.log(`json = ${json}`);
+    return repos = json;
+})
+.then((repos)=>{
+    console.log(`repos = ${repos}`);
+    // Object.keys(myArray).length
+    console.log(`json.length = ${Object.keys(repos).length}`);
+    console.log(`Button[0] = ${repos.ToolBar.Button[0]}`);
+    console.log(`ToolBar.Button[1].name = ${repos.ToolBar.Button[1].name}`);
+    console.log(`ToolBar.Button[1].Captio = ${repos.ToolBar.Button[1].Caption}`);
+    console.log(`name = ${repos.name}`);
+    console.log(`name = ${repos.caption}`);
+    console.log(`ToolBar.Caption = ${repos.ToolBar.Caption}`);
+    console.log(`ToolBar.name= ${repos.ToolBar.name}`);
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
